684188baa4a047a014966cb5b041c744969bc6c1
Paul Mölders <paulmoelders@gmail.com>
2016-12-19 10:08:41 +0100

    adds hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: {
+        bundle: [
+            "webpack-dev-server/client?http://localhost:8080",
+            "webpack/hot/only-dev-server",
+            "./src/index.tsx"
+        ],
+        vendor: [
+            "react",
+            "react-dom",
+            "redux",
+            "react-redux"
+        ]
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: __dirname + "/dist",
         publicPath: "dist"
     },
@@ -16,22 +31,22 @@ module.exports = {
 
     module: {
         loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            {
+                test: /\.tsx?$/,
+                include: path.join(__dirname, 'src'),
+                loaders: ["react-hot", "ts-loader"]
+            }
         ],
 
         preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
+            {
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")
+    ]
 };
